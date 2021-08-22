d4f83b33b9bbddeaeddf8a99ed792a7969ddfac9
minuk926 <minuk926@gmail.com>
2021-08-23 03:42:41 +0900

    study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Subject from './components/Subject';
+import TOC from './components/TOC';
+import Content from './components/Content';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        selectedToc: 0,
+        subject : {title: "WEB", desc: "World wide web!!!"},
+        list: [
+            {id: 1, title: "HTML", desc: "HyperText Markup Language."},
+            {id: 2, title: "CSS", desc: "Cascading Style Sheets."},
+            {id: 3, title: "JavaScript", desc: "Java Script Language."}
+        ],
+        content: {title: "HTML", desc: "HTML is HyperText Markup Language."},
+
+    };
+
+  }
+  render() {
+      let _title, _desc = null;
+      _title = this.state.content.title;
+      _desc= this.state.content.desc;
+
+      let i = 0;
+      let data = null;
+
+      while(i < this.state.list.length){
+          data = this.state.list[i];
+          if(this.state.selectedToc === data.id) {
+              _title = data.title;
+              _desc = data.desc;
+              break;
+          }
+          i += 1;
+      }
+
+    return (
+        <div className="App">
+            <Subject title={this.state.subject.title} desc={this.state.subject.desc}></Subject>
+            <TOC data={this.state.list}
+                 onChangePage={function(id){
+                     debugger
+                    this.setState({
+                        selectedToc: Number(id)
+                    });
+                 }.bind(this)}
+            >
+            </TOC>
+            <Content title={_title} desc={_desc}></Content>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TOC.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+
+class TOC extends Component {
+
+	render(){
+		let data = this.props.data;
+		let i = 0;
+        let list = [];
+        while(i < data.length){
+			list.push(<li key={data[i].id}><a href={"/content/"+data[i].id+".html"}
+				data-id={data[i].id}
+				onClick={
+				function(id, e){
+					e.preventDefault();
+					//debugger;
+					// this.setState({
+					// 	seleletedToc: e.target.dataset.id
+					// });
+					this.props.onChangePage();
+					//this.props.onChangePage(e.target.dataset.id);
+
+				}.bind(this, data[i].id)
+			}>{data[i].title}</a></li>);
+        	i += 1;
+		}
+
+		return (
+			<nav>
+				<ul>
+					{list}
+				</ul>
+			</nav>
+		)
+	};
+}
+
+export default TOC;
