70d96c8f17a5219817554ca46a4071af9e7bdc2d
julim <minuk926@inpix.com>
2021-08-24 00:08:59 +0900

    create & update 테스트

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     this.max_content_id = 3;
     this.state = {
         mode: 'read',
-        selectedToc: 1,
+        selected_toc_id: 1,
         subject : {title: "WEB", desc: "World wide web!!!"},
         contents: [
             {id: 1, title: "HTML", desc: "HyperText Markup Language."},
@@ -40,7 +40,7 @@ class App extends Component {
                   // })
 
                   // 2 - 제외
-                  // let cpArr = Array.from(this.state.contents);
+                  // let cpArr = Array.from(this.state.contents);  Objec.assign
                   // cpArr.push(
                   //     {id: this.max_content_id, title: title, desc: desc}
                   // );
@@ -50,7 +50,9 @@ class App extends Component {
 
                   // 3 - spread 연산자
                   this.setState({
-                      contents: [...this.state.contents, {id: this.max_content_id, title: title, desc: desc}]
+                      contents: [...this.state.contents, {id: this.max_content_id, title: title, desc: desc}],
+                      selected_toc_id: this.max_content_id,
+                      mode: 'read'
                   });
 
                   // // 4 - concat 연산
@@ -61,21 +63,27 @@ class App extends Component {
           }/>;
       else if (this.state.mode === 'update') {
           _article = <UpdateContent data={this.getReadContent()} onAction={
-
               (id, title, desc) => {
-                 this.setState({
-                     contents: [...this.state.contents, {id: id, title: title, desc: desc}]
-                 })
+                  //let _contents = Array.from(this.state.contents);
+                  //let _contents = Object.assign([], this.state.contents);
+                  let _contents = [...this.state.contents];
+                  _contents[id - 1] = {id: id, title: title, desc: desc};
+
+                  this.setState({
+                      contents: _contents,
+                      mode: 'read'
+                  })
               }
           }/>;
       }
+
     return (
         <div className="App">
-            <Subject title={this.state.subject.title} desc={this.state.subject.desc}></Subject>
+            <Subject title={this.state.subject.title} desc={this.state.subject.desc}/>
             <TOC data={this.state.contents}
                  onChangePage={function(id){
                     this.setState({
-                        selectedToc: id,
+                        selected_toc_id: id,
                         mode: 'read'
                     });
                  }.bind(this)}
@@ -83,9 +91,35 @@ class App extends Component {
             </TOC>
             <Control onChangeMode={
                 (mode)=>{
-                    this.setState({
-                        mode: mode
-                    })
+                    if(mode === 'delete'){
+                        if(window.confirm('삭제하시겠습니까?')){
+                            let _contents = [...this.state.contents];
+
+                            let i = 0;
+                            let data = null;
+
+                            while (i < _contents.length) {
+                                data = _contents[i];
+                                if (this.state.selected_toc_id === data.id) {
+                                    _contents.splice(i, 1);
+                                    break;
+                                }
+                                i += 1;
+                            }
+
+                            this.setState({
+                                mode: mode,
+                                contents: _contents,
+                                selected_toc_id: this.state.contents.length > 0? 1: 0
+                            });
+                            alert('삭제 되었습니다');
+                        }
+
+                    }else{
+                        this.setState({
+                            mode: mode,
+                        })
+                    }
                 }
             }
             />
@@ -101,7 +135,7 @@ class App extends Component {
 
         while (i < this.state.contents.length) {
             data = this.state.contents[i];
-            if (this.state.selectedToc === data.id) {
+            if (this.state.selected_toc_id === data.id) {
                 return data;
             }
             i += 1;
--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,30 +1,41 @@
 import React, {Component} from "react";
 
 class UpdateContent extends Component {
-	shouldComponentUpdate(nextProps, nextState, nextContext) {
-		return nextProps.data !== this.props.data;
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: this.props.data.title,
+			desc: this.props.data.desc
+		};
+		this.inputFormHandler = this.inputFormHandler.bind(this);
 	}
+
+	inputFormHandler(e){
+		this.setState({[e.target.name]: e.target.value});
+	}
+	// shouldComponentUpdate(nextProps, nextState, nextContext) {
+	// 	return nextProps.data !== this.props.data;
+	// }
 	render() {
 		console.log('#####> UpdateContent render');
 		return (
 			<article>
 				<h2>Update</h2>
-				<form action="/create_process" method="post" onSubmit={
+				<form action="/" method="post" onSubmit={
 					function(e){
 						e.preventDefault();
-						this.props.onAction(e.target.id.value, e.target.title.value, e.target.desc.value);
+						this.props.onAction(this.props.data.id, this.state.title, this.state.desc);
 					}.bind(this)
 				}>
 					<p>
-						<input type="text" name="title" placeholder="title" value={this.props.data.title}/>
+						<input type="text" name="title" placeholder="title" value={this.state.title} onChange={this.inputFormHandler}/>
 					</p>
 					<p>
-						<textarea name="desc" placeholder="description" value={this.props.data.desc}></textarea>
+						<textarea name="desc" placeholder="description" value={this.state.desc} onChange={this.inputFormHandler}/>
 					</p>
 					<p>
 						<input type="submit"/>
 					</p>
-					<input name="id" type="hidden" value={this.props.data.id}/>
 				</form>
 			</article>
 		)
